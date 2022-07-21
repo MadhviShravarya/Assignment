8ea3e15e6b3e304d07854a1a96988de9449b6f33
Madhvi Pandey <109618638+MadhviShravarya@users.noreply.github.com>
2022-07-21 14:15:29 +0530

    Add files via upload

---
+++ 10.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c;
+	printf("(",a);
+	scanf("%d",&a);
+	
+	printf(", ",b);
+	scanf("%d",&b);
+	printf(", ",c);
+	scanf("%d",&c);
+	printf(" )");
+	
+	printf("(Day-%02d,Month-%02d,Year-%d)",a,b,c );
+	
+	return 0;
+}
